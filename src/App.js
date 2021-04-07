90286b908f4c192389cab6a09557890cf292b0b9
Mohammed Islam <mmislam101@gmail.com>
2021-04-06 20:21:26 -0700

    Initial example components and material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Counter from './components/Counter'
+import { Button } from '@material-ui/core';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementer() {
+    let currentCount = this.state.count;
+    currentCount++;
+
+    this.setState(state => ({
+      count: currentCount
+    }));
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          Hello {this.props.name}
+        </div>
+        <Counter count={ this.state.count }/>
+        <Button onClick={() => { this.incrementer() }} >Primary</Button>
+      </>
+    );
+  }
 }
 
 export default App;
